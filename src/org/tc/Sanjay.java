bfa18f53d1643656f6fd88dda337e4d35d227575
S-sanjay-07 <sanjaytvs30@gmail.com>
2024-03-27 11:26:09 +0530

    new code

---
+++ src/org/tc/Sanjay.java
@@ -0,0 +1,26 @@
+//package org.tc;
+//
+//public class sanjay {
+//	private void sa() {
+//		System.out.println("Sanjay");
+//
+//	}
+//
+//	public static void main(String[] args) {
+//		sanjay saaa= new sanjay();
+//		saaa.sa();
+//	}
+//
+//}
+package org.tc;
+
+public class Sanjay {
+    private void sa() {
+        System.out.println("Sanjay");
+    }
+
+    public static void main(String[] args) {
+        Sanjay saaa = new Sanjay();
+        saaa.sa();
+    }
+}
